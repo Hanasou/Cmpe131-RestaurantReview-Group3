5f0a034b7ce4b58718aff3c9614aef41fa38ede3
LetTheBassKick <royzhang42@gmail.com>
2018-04-07 13:02:27 -0700

    Create AppDemo.java

---
+++ LogIn/AppDemo.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class AppDemo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AppDemo
+{
+  public static void main(String[] args)
+  {
+        LogInManager manager = new LogInManager();
+        manager.createLogInPage();
+   }
+}
